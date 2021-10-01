8f95a913fec1695e90b7e7c31f2b82164389894a
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-10-01 23:23:34 +0700

    Heroku Test 2

--- src/views/404.tsx
+++ src/views/404.tsx
@@ -1,3 +1,3 @@
 export default function PageNotFound() {
-  return <h1 className="mx-auto text-center">Page Not Found</h1>;
+  return <h1 className="mx-auto text-center pt-5">Page Not Found</h1>;
 }
