f71ba8f01764365a5bdad37a4941b2e2fed6697d
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-10-02 00:48:32 +0700

    Seperate Backend

--- src/utils/Quote.ts
+++ src/utils/Quote.ts
@@ -10,14 +10,14 @@ export interface fullQuoteResponse extends quoteResponse {
 
 export async function randomQuote(percent: number): Promise<string> {
   const res = await axios.get<quoteResponse>(
-    `http://localhost:3030/api/quote?percent=${percent}`
+    `https://food-waste-quotes-api.herokuapp.com/quote?percent=${percent}`
   );
   return res.data.quote;
 }
 
 export async function getAllQuote(): Promise<fullQuoteResponse[]> {
   const res = await axios.get<fullQuoteResponse[]>(
-    "http://localhost:3030/api/allquotes?password=1234"
+    "https://food-waste-quotes-api.herokuapp.com/allquotes?password=1234"
   );
   return res.data;
 }
